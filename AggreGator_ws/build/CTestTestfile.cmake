c1730742ee0bc356da8e1ee0be8b7ccc9d963050
Daniel Kelly <dkelly6793@yahoo.com>
2014-02-22 17:02:01 -0500

    Testing new workspace protocol

---
+++ AggreGator_ws/build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/viki/GIThub/NASAboticsCode/AggreGator_ws/src
+# Build directory: /home/viki/GIThub/NASAboticsCode/AggreGator_ws/build
+# 
+# This file includes the relevent testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+SUBDIRS(gtest)
---
+++ AggreGator_ws/build/catkin_generated/order_packages.py
@@ -0,0 +1,4 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = "/home/viki/GIThub/NASAboticsCode/AggreGator_ws/src"
+whitelisted_packages = "".split(';') if "" != "" else []
+blacklisted_packages = "".split(';') if "" != "" else []
