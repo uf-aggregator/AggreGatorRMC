c20b83c75e41a179463c9c4d0c933d1a8732f385
Kevin French <fnivek@gmail.com>
2014-03-11 22:49:58 -0400

    Set up a linear actuator node and a bucket motor node

--- AggreGator_ws/devel/share/remote_control/cmake/remote_control-msg-extras.cmake
+++ AggreGator_ws/devel/share/remote_control/cmake/remote_control-msg-extras.cmake
@@ -1,2 +1,2 @@
-set(remote_control_MESSAGE_FILES "/home/viki/GIThub/NASAboticsCode/AggreGator_ws/src/remote_control/msg/motorMSG.msg")
+set(remote_control_MESSAGE_FILES "/home/fnivek/git_hub/NASAboticsCode/AggreGator_ws/src/remote_control/msg/motorMSG.msg")
 set(remote_control_SERVICE_FILES "")
