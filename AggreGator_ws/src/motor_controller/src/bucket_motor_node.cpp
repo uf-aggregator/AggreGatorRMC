bf4d98765d13199a5e00332446e4272b5dd48cf4
Kevin French <fnivek@gmail.com>
2014-03-12 21:08:53 -0400

    Edited the Linear_actuator_node and the Bucket_motor_node

--- AggreGator_ws/src/motor_controller/src/bucket_motor_node.cpp
+++ AggreGator_ws/src/motor_controller/src/bucket_motor_node.cpp
@@ -2,33 +2,47 @@
 #include "std_msgs/builtin_int16.h"
 #include "motor_controller/I2CMSG.h"
 
-void callback (const std_msgs::Int16& msg)
-{
+ros::Subscriber sub;
+ros::Publisher pub;
 
+motor_controller::I2CMSG GenerateMSG(int setpoint)
+{
+    motor_controller::I2CMSG msg;
+    //Set msg addr
+    //msg.addr = ???;           //Use an enumerated type from the I2C node
+
+    //Set the msg data
+    msg.data = setpoint;
+
+    return msg;
+}
+
+motor_controller::I2CMSG BucketMotorPID(int setpoint)
+{
+    /*
+     * This is where the pid controller goes
+     */
+
+    return GenerateMSG(setpoint);               //Output the msg NOTE: change the passed parameter do not use setpoint directly
+}
+
+void SetSetpoint(const std_msgs::Int16& msg)
+{
+    pub.publish(BucketMotorPID(msg.data));   //Generate and publish an I2C msg
 }
 
 //MAIN
 int main(int argc, char** argv)
 {
-	ros::init(argc, argv, "bucket_motor_node"); //Initialize node
+    ros::init(argc, argv, "bucket_motor_node");                              //Initialize node
+    ros::NodeHandle n;                                                       //Create nodehandle object
 
-	ros::NodeHandle n; //Create nodehandle object
+    sub = n.subscribe("bucket_motor_rc", 1000, SetSetpoint);                 //Create object to subscribe to topic "linear_actuator_rc"
+    pub = n.advertise<motor_controller::I2CMSG>("I2C",1000);                 //Create object to publish to topic "I2C"
 
-	ros::Subscriber sub = n.subscribe("bucket_motor_rc", 1000, callback); //Create object to subscribe to topic "bucket_motor_rc"
-	
-	ros::Publisher pub = n.advertise<motor_controller::I2CMSG>("I2C",1000); //Create object to publish to topic "I2C"
+    //Loop as long as ros system is ok
+    ros::spin();
 
-	ros::Rate loop_rate(500); //Set frequency of looping. 10 Hz
-	
-	//Loop is true while ctrl-c isn't pressed, or ros::shutdown() has not been called, or the node has not been kicked off the network.
-	while(ros::ok()){
 
-		ros::spinOnce(); //Call the callback function
-		
-		loop_rate.sleep(); //Sleep for the time remaining to allot 10 Hz publishing rate.
-
-	}
-
-	
-	return 0;
+    return 0;
 }
