2b1378b4ed1a2248f500666edc59fae23bd5465a
abrarmp <abrarmp@gmail.com>
2014-03-26 21:15:37 -0400

    cleaned up ina226 source

--- AggreGator_ws/src/pinInterface/ina226.c
+++ AggreGator_ws/src/pinInterface/ina226.c
@@ -8,70 +8,58 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+#include "i2c.h"
 
-
-void main(void) {
-    int file;
-    char filename[40];
+int main(void) {
+    int file; //the "file" that will be used to interface with the i2c bus
+    char filename[40]; //name of said file
+    int i2c_status = 0; 
+    const char *error_buffer; //buffer to store error messages
+	char read_buffer[3] = {0}; //buffer to store flag and data from device 
+    float data; //to be used to store data once read	
+	int addr = 0x40;        // The I2C address of the ADC
     char reg_ptr_addr[1] = {0x02}; //bus voltage register address
-    const char *buffer;
-    int addr = 0x40;        // The I2C address of the ADC
-    
-    char config_reg[3] = {0}; //address is 0
-    config_reg[1] = 0x05; //high byte 0b00001111
-    config_reg[0] =  0x27; //low byte 0b11100111;
+    char config_reg[3] = {0}; //address of configuration register is 0x00
+//set configuration values -- see datasheet for what each bit means
+//DEFAULT: 0b0100000100100111
+//15- reset, equivalent to power on
+//14:12- reserved
+//11:9- number of averages
+//8:6- conversion time
+//5:3- shunt voltage conversion time
+//2:0- operating mode
 
-//open the file to interface with the i2c bus
+//1024 averages, 1.1ms conversion time, continuous bus voltage
+	config_reg[1] = 0b01001111; //high byte 
+    config_reg[2] = 0b00100110; //low byte 
+
+//write the name of the file that needs to be opened to interface with the i2c bus	
     sprintf(filename,"/dev/i2c-1");
-    if ((file = open(filename,O_RDWR)) < 0) {
-        printf("Failed to open the bus.");
-        /* ERROR HANDLING; you can check errno to see what went wrong */
-        exit(1);
-    }
 
-//see if there's a device at "addr"
-    if (ioctl(file,I2C_SLAVE,addr) < 0) {
-        printf("Failed to acquire bus access and/or talk to slave.\n");
-        /* ERROR HANDLING; you can check errno to see what went wrong */
-        exit(1);
-    }
-
-    char buf[10] = {0};
-    float data;
-    char channel;
-
-    if(write(file, config_reg, 3) != 3) {
-    
-	//ERROR HANDLING: i2c transaction failed
-	printf("Failed to write to the i2c bus.\n\n");
-    }     
-
-    if(write(file,reg_ptr_addr,1) != 1) {
-        /* ERROR HANDLING: i2c transaction failed */
-        printf("Failed to write to the i2c bus.\n\n");
-   //     buffer = g_strerror(errno);
-//        printf(buffer);
-      printf("\n\n");
-    }
-    
-
-    int i = 0;
-    while(1)//for(i = 0; i<4; i++) {
-      {  // Using I2C Read
-        if (read(file,buf,2) != 2) {
-            /* ERROR HANDLING: i2c transaction failed */
-            printf("Failed to read from the i2c bus.\n");
-     //       buffer = g_strerror(errno);
-    //        printf(buffer);
-            printf("\n\n");
-        } else {
-            data = (float)(buf[0]<<8)+buf[1];
+//write configuration settings
+	i2c_status = writetoreg_i2c(addr, config_reg[0], 2, config_reg+1);
+	if(i2c_status == 1)
+		return 1;
+		
+//set the register to be read from
+    i2c_status = write_i2c(addr, 1, &reg_ptr_addr);
+    if(i2c_status == 1)
+		return 1;
+		
+//continuously read   
+    while(1)
+	{  
+        read_buffer = read_i2c(addr, 2); 
+		if(read_buffer[0]==1)
+			return 1;
+		else
+		{
+            data = (float)(read_buffer[1]<<8)+read_buffer[2];
             data = data*1.25/1000;
-            //channel = ((buf[0] & 0b00110000)>>4);
             system("clear");
 	    printf("Data:  %04f V\n",data);
 	    usleep(500000);	
-	 }
+		}
     }   
 }
 
