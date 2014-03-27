2c52027b00afe1f0e81bf1557fdcaeb8dfb5af07
FaytxZen <faytxzen@faytxzen-X502CA.(none)>
2014-03-27 17:36:38 -0400

    Renamed files. Added separate nodes for printing and normal range coords. Updated Cmake.

---
+++ AggreGator_ws/src/ladar/src/ladarRange.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+#include "ros/ros.h"
+#include "std_msgs/String.h"
+#include "sensor_msgs/LaserScan.h"
+
+float* getX(){
+	
+	}//end getX
+
+float* getY(){
+	
+	}//end getY
+
+
+	
+int main(int argc, char **argv){
+	ros::init(argc, argv, "ladar_range_node");
+	
+	ros::NodeHandle n;
+	
+	ros::Subscriber sub = n.subscribe("/scan", 1, getY);//change callback function
+
+	ros::spin();
+	
+	return 0;
+}
