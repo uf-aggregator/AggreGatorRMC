18083b6297a1e6fcdef8c382d262e0a66e333804
Daniel Kelly <dkelly6793@yahoo.com>
2014-05-16 13:27:24 -0400

    Updated power monitoring node to include remaining power and relative pathing.

--- AggreGator_ws/src/power_monitoring/src/power_monitoring_node.cpp
+++ AggreGator_ws/src/power_monitoring/src/power_monitoring_node.cpp
@@ -24,16 +24,18 @@ ros::Duration update_rate(15); //time in seconds between power monitoring update
 int TOTAL_JOULES = 133200;
 
 //global battery percentage value
-float multi_energy = 0.00;		//Stores energy over multiple runs
-float current_energy = 0.00; 		//store energy over one run
+float multi_energy = 0.00;		//Stores energy over multiple runs, percentage
+float current_energy = 0.00; 		//store energy over one run, percentage
+float remaining_energy = 100.00;	//Remaining energy left in battery, percentage
 
 /*Write percentage to file*/
 bool writePercentage(float percentage_1, float percentage_2){
-	ofstream batteryFile("../../../../../BatteryPercentage.txt", ios::trunc | ios::out);
+	ofstream batteryFile("../../../../BatteryPercentage.txt", ios::trunc | ios::out);
 	if(batteryFile.is_open())
-	{
+	{	
+		batteryFile << "UF AggreGator, Percent of Electronics Battery Used. \n";
 		batteryFile << "Single Run: " << percentage_1 << "\t"; 
-		batteryFile << "Overall Total: " << percentage_2 << "\t";
+		batteryFile << "Overall Total: " << percentage_2;
 		batteryFile.close();
 		return true;
 	}
@@ -46,16 +48,18 @@ bool writePercentage(float percentage_1, float percentage_2){
 /*Read percentage from file and then set global multi_energy to that value*/
 bool readPercentage(){
 	string percentage;
-	fstream batteryFile("../../../../../BatteryPercentage.txt", ios::in);
+	fstream batteryFile("../../../../BatteryPercentage.txt", ios::in);
 	if(batteryFile.is_open()){
+		batteryFile.seekg(88,ios::beg); //Set stream pointer to read correct float value
 		while(getline(batteryFile, percentage)){
 			multi_energy = (float)atof(percentage.c_str());
+			remaining_energy = 100.00 - multi_energy;
+			//ROS_INFO("multi_energy %f",multi_energy);
 		}//endwhile	
 		batteryFile.close();
 		return true;
 	} else {
 		ROS_WARN("Battery log can't be opened, assuming 0.");
-		batteryFile << "UF AggreGator Mining Team, Percentage of Electronics Battery Used \n";
 		return false;
 	}
 }
@@ -73,51 +77,39 @@ void TrackElectronicPower(const hardware_interface::ElectronicPowerData& data)
 {
 	float calculatedPower = data.power; //Calculates power based on the digital value from the INA226 and the bit-to-Watt conversion ratio. Calculated in the ina226 node
 	//ROS_INFO("%i",data.power);
-	
 	current_time = ros::Time::now();
 	
 	float percent = (calculatedPower * (current_time - last_callback).toSec()) * 100 / TOTAL_JOULES;
 	current_energy += percent;
 	multi_energy += percent;
-	
+	remaining_energy = 100 - multi_energy;
+	if(remaining_energy <= 20)
+		ROS_ERROR("Low Battery! Consider charging electronics battery.");
 	last_callback = current_time;
-	
 	//ROS_INFO("actual power: %f",calculatedPower);
 	
 }
 
-//Adds together all power readings to get total power usage, resets the powerReadings vector with the updated value and saves the total power reading to a file for logging purposes
+//Outputs power data for electronics and writes data to a file for logging purposes
 void UpdateElectronicPowerUsage()
 {
-	
+
 	ROS_INFO("Power used (this run : multiple runs) = %.2f%% : %.2f%%", current_energy, multi_energy);
+	ROS_INFO("Remaining Power = %.2f%% ", remaining_energy);
 	writePercentage(current_energy,multi_energy);
+	
 	
 }
 int main(int argc, char** argv)
 {
     /*
-     * Initilization
+     * Initialization
      */
 
     //Initilize the power monitoring node
     ros::init(argc, argv, "power_monitoring_node");
+		
 	
-	//Setup file for power logging
-	ofstream powerFile;
-	powerFile.open("~/PowerUsageLog.txt", ios::out | ios::trunc);
-	if(powerFile.is_open())
-	{	
-		powerFile << "UF AggreGator Mining Team, Electronics Power Logged during a Competition Run. \n";
-		powerFile << "This file shows total power used up to the indicated time. \n";
-		powerFile << "Value on the left is time in seconds since the system was running. \n";
-		powerFile << "Value on the right is the total power used up to the indicated time duration. \n";
-		powerFile.close();
-	}
-	else
-		ROS_ERROR("Cannot open file for power logging!");
-		
-		
     //Node handler this is how you work with ROS
     ros::NodeHandle n;
 
@@ -129,7 +121,7 @@ int main(int argc, char** argv)
     last_time = start_time;
     last_callback = start_time;
 
-    //Initilize power
+    //Initialize power
     readPercentage();
 
     /*
